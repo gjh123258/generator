b71a026d57fc781ef39d68d948bb51c4932be704
gjh <gjh123258@163.com>
2020-12-30 11:47:29 +0800

    代码生产器

---
+++ src/main/java/com/mygeneator/generator/MyGeneator.java
@@ -0,0 +1,95 @@
+package com.mygeneator.generator;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * @Title: MyGeneator
+ * @Description:
+ * @Auther:
+ * @Version: 1.0
+ * @create 2020/12/24 9:26
+ */
+public class MyGeneator {
+    public static void main(String[] args) {
+        // 构建一个代码生成对象
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 1. 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        //D:\dongbao\上课\msb-dongbao-mall\msb-dongbao-mall-parent-v1\msb-dongbao-service\msb-dongbao-ums\src\main\java
+        String path = "D:\\workspace\\rxdrepairsaas-labcenter\\src\\main\\java";
+        String cloumnName = "inspection_";
+
+        String separator = File.separator;
+        gc.setOutputDir(path);
+        gc.setAuthor("gjh");
+        gc.setOpen(false);//打开目录
+        gc.setFileOverride(true);//是否覆盖
+        gc.setServiceName("%sService");//去Service的I前缀。
+        gc.setIdType(IdType.ID_WORKER);
+        gc.setDateType(DateType.ONLY_DATE);
+        gc.setSwagger2(true);//开启Swagger2
+
+        mpg.setGlobalConfig(gc);
+
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://10.192.33.171:3306/lab_center?useSSL=false&useUnicode=true&characterEncoding=utf-8&serverTimezone=Asia/Shanghai");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("mysql171171");
+        dsc.setDbType(DbType.MYSQL);
+
+        mpg.setDataSource(dsc);
+
+        // 包设置
+        PackageConfig pc = new PackageConfig();
+
+        pc.setParent("com.rxdrepairsaas.labcenter.inspection");
+        pc.setEntity("entity");
+        pc.setMapper("mapper");
+        pc.setController("controller");
+
+        mpg.setPackageInfo(pc);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setInclude(cloumnName);//表名
+        strategy.setNaming(NamingStrategy.underline_to_camel);// 下划线转他驼峰
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);// 列 下划线转脱发
+        strategy.setEntityLombokModel(true);//lombok 开启
+        strategy.setLogicDeleteFieldName("deleted");
+
+        // 自动填充
+        TableFill gmtCreate = new TableFill("create_time", FieldFill.INSERT);
+        TableFill gmtModify = new TableFill("update_time",FieldFill.INSERT_UPDATE);
+        ArrayList<TableFill> tableFills = new ArrayList<>();
+        tableFills.add(gmtCreate);
+        tableFills.add(gmtModify);
+
+        strategy.setTableFillList(tableFills);
+
+        //乐观锁
+        strategy.setVersionFieldName("version");
+
+        // restcontroller
+        strategy.setRestControllerStyle(true);
+        strategy.setControllerMappingHyphenStyle(true);// localhost:xxx/hello_2
+
+        mpg.setStrategy(strategy);
+
+        mpg.execute();
+    }
+}
